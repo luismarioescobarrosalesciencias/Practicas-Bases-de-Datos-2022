70dbae09317e4ad4aed067a18772826e6dd77d6c
Luis Mario <luismescobar@ciencias.unam.mx>
2022-06-15 09:30:10 -0500

    Archivo generador csv

--- ProyectoFInal/programasAuxliares/trabajandocsv.py
+++ ProyectoFInal/programasAuxliares/trabajandocsv.py
@@ -9,22 +9,26 @@ def id_generator(size, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 sol=""
 i = 0
-while (i <= 300):
+while (i <= 2000):
     sol = id_generator(18)
     if sol not  in uniques:
         uniques.add(sol)
         i+=1
+print("UNICOS")
+#for x in uniques :
+#    print(x)
 
-for x in uniques :
-    print(x)
+
 #tomo unos 50 de persona y los usamos para empleado y cliente , los demas see dvidiran
 #guardamos csv
-guarda = uniques  #guarda nuestros unicos
+guarda = set()  #guarda nuestros unicos
+for x in uniques:
+    guarda.add(x)
+
 
 j =  0
 interseccionCE = set ()
-
-while(j<= 50):
+while(j<= 100):
     s = uniques.pop()
     interseccionCE.add(s)
     j+=1
@@ -35,7 +39,7 @@ print(".........................................................................
 #guardamos
 empleados = set()
 k=1
-while(k<=125):
+while(k<=1000):
     s = uniques.pop()
     empleados.add(s)
     k+=1
@@ -51,3 +55,78 @@ clientes = clientes.union(interseccionCE)
 #print(len(clientes))
 #print(len(uniques))
 #print(len(empleados.union(clientes)))
+print("longitud")
+
+if empleados.union(clientes) == guarda:
+    print("okey")
+else:
+    print("MALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+    print(empleados.union(clientes).difference(guarda))
+    print("mallllllllllllllllllllllll")
+    print(guarda.difference(empleados.union(clientes)))
+
+
+#escribir un archivo para guarda
+#escribir un archvo para empleados
+#escribir un archivo para los derivados de empleados
+def escribe(nombre,conjunto):
+    with open(nombre, 'w') as f:
+        for x in conjunto:
+            f.write(x)
+            f.write('\n')
+
+
+escribe('Persona.txt',guarda)
+escribe('Cliente.txt',clientes)
+escribe('Empleados.txt',empleados)
+'''
+with open('Persona.txt', 'w') as f:
+    for x in guarda:
+        f.write(x)
+        f.write('\n')
+
+with open('Cliente.txt', 'w') as f:
+    for x in clientes:
+        f.write(x)
+        f.write('\n')
+
+with open('Empleados.txt', 'w') as f:
+    for x in guarda:
+        f.write(x)
+        f.write('\n')
+'''
+
+#dividir empleados
+taquero = set () #230
+parrillero = set () #200
+cajero = set () # 70 cajeros
+tortillero = set ()
+mesero = set () #200
+repartidor = set () #80
+
+
+
+def dividir(conjunto1,conjunto2,partes):
+    count = 0 ;
+    while(count <partes):
+        s = conjunto1.pop()
+        conjunto2.add(s)
+        count+=1
+
+dividir(empleados,taquero,230)
+escribe('taquero.txt',taquero)
+
+dividir(empleados,parrillero,220)
+escribe('parrillero.txt',parrillero)
+
+dividir(empleados,cajero,80)
+escribe('cajero.txt',cajero)
+
+dividir(empleados,mesero,240)
+escribe('mesero.txt',mesero)
+
+dividir(empleados,repartidor,100)
+escribe('repartidor.txt',repartidor)
+
+dividir(empleados,tortillero,len(empleados))
+escribe('tortillero.txt',tortillero)
